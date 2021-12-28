53532f34e17b7337d0d288158f8cb5d1d9175895
fuli <>
2021-12-28 20:48:15 +0800

    add. some. res

---
+++ Assets/3rdParty/2D_Destruction/Scripts/ExplodeOnClick.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Explodable))]
+public class ExplodeOnClick : MonoBehaviour
+{
+
+    private Explodable _explodable;
+
+    void Start()
+    {
+        _explodable = GetComponent<Explodable>();
+    }
+
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            OnMouseDown();
+        }
+    }
+
+    void OnMouseDown()
+    {
+        _explodable.explode();
+        // ExplosionForce ef = GameObject.FindObjectOfType<ExplosionForce>();
+        // ef.doExplosion(transform.position);
+    }
+}
---
+++ Assets/UI/异步加载场景进度条Slider/ReadMe.txt
@@ -0,0 +1,3 @@
+该方法用来实现游戏加载进度条
+注意事项:
+	1、Scene导入到buildingSetting，顺序：StartScene 、LoadingScene、MainScene
