263c45f265d23f17ccb64d087479026802be06ab
fuli <>
2022-07-19 16:47:53 +0800

    remove sdkadapter

---
+++ Assets/UI/超链接/Demo/testScrollView.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class testScrollView : MonoBehaviour
+{
+    public GameObject goPrefab;
+
+    public void Add()
+    {
+        if (goPrefab)
+        {
+            GameObject go = Instantiate(goPrefab);
+            go.transform.SetParent(goPrefab.transform.parent);
+
+            // LinkImageText text = go.GetComponentInChildren<LinkImageText>();
+            // text.text = "<quad name=xb_a size=25 width=1 />";
+            // if (Random.value > 0.5f)
+            // {
+            //     text.text += text.text;
+            // }
+        }
+    }
+}
